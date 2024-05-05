51c22681ba69fb8d898515055d0034c1746e7898
JOHN-AMAO <amaojohn767@gmail.com>
2024-05-05 23:01:26 +0100

    .

--- app/(home)/dashboard/page.tsx
+++ app/(home)/dashboard/page.tsx
@@ -1,6 +1,8 @@
+"use client";
 import ActionTypeList from "@/components/ActionTypeList";
-
+import { useSession } from "next-auth/react";
 const Home = () => {
+  const session = useSession();
   const now = new Date();
 
   const time = now.toLocaleTimeString("en-US", {
@@ -14,12 +16,12 @@ const Home = () => {
   return (
     <section className='flex size-full flex-col gap-5 text-white'>
       <div className='h-[303px] w-full rounded-[20px] bg-hero bg-cover'>
-        <div className='flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11'>
-          <h2 className='glassmorphism max-w-[273px] rounded py-2 text-center text-base font-normal'>
-            Upcoming Meeting at: 12:30 PM
+        <div className='flex h-full flex-col justify-between max-md:py-8 lg:p-11'>
+          <h2 className='glassmorphism lg:text-3xl font-extrabold rounded py-2 '>
+            {session.data?.user?.name}
           </h2>
           <div className='flex flex-col gap-2'>
-            <h1 className='text-4xl font-extrabold lg:text-7xl'>{time}</h1>
+            <h1 className='text-3xl font-extrabold lg:text-5xl'>{time}</h1>
             <p className='text-lg font-medium text-sky-1 lg:text-2xl'>{date}</p>
           </div>
         </div>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,15 +11,17 @@ const Navbar = () => {
         href='/'
         className='flex items-center gap-1'
       >
-        <Image
+        {/* <Image
           src='/icons/logo.svg'
           width={32}
           height={32}
-          alt='yoom logo'
+          alt='spectrum logo'
           className='max-sm:size-10'
-        />
-        <p className='text-[26px] font-extrabold text-white max-sm:hidden'>
-          Spectrum Finance.
+        /> */}
+        <p className='text-[26px] font-extrabold font-sans border-b-2 max-sm:hidden'>
+          <span className='bg-gradient-to-r from-blue-500 to-purple-600 text-transparent bg-clip-text'>
+            Spectrum
+          </span>
         </p>
       </Link>
       <div className='flex-between gap-5 '>
