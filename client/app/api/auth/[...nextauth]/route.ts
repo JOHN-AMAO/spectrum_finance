3c7752c1a5f13f2c9a604eb9d3c0561471f8bf7e
JOHN-AMAO <amaojohn767@gmail.com>
2024-05-12 14:15:47 +0100

    fixing authoptions bug

--- client/app/api/auth/[...nextauth]/route.ts
+++ client/app/api/auth/[...nextauth]/route.ts
@@ -6,7 +6,7 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
 import prisma from "@/app/libs/prismadb";
 
-export const authOptions: AuthOptions = {
+const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
@@ -56,4 +56,4 @@ export const authOptions: AuthOptions = {
 
 const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST };
+export { handler as GET, handler as POST, authOptions };
